6e317bc03bc01d6e521b9036e6ca36999fc2a5e7
Guido <iguido@me.com>
2026-01-18 16:44:24 +0100

    1 six-pack requirement

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -7,16 +7,42 @@ class Bottles
     upper.downto(lower).map { |i| verse(i) }.join("\n")
   end
 
-  def verse(number)
+  def container(number)
     case number
     when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+      "no more bottles of beer"
     when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+      "1 bottle of beer"
+    when 6
+      "1 six-pack of beer"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+      "#{number} bottles of beer"
     end
   end
+
+  def next_action(number)
+    case number
+    when 0
+      "Go to the store and buy some more"
+    when 1
+      "Take it down and pass it around"
+    else
+      "Take one down and pass it around"
+    end
+  end
+
+  def next_number(number)
+    case number
+    when 0
+      99
+    else
+      number - 1
+    end
+  end
+
+  def verse(number)
+    "#{container(number).capitalize} on the wall, #{container(number)}.\n#{next_action(number)}, #{container(next_number(number))} on the wall.\n"
+  end
+
+
 end
--- bottles/test/bottles_test.rb
+++ bottles/test/bottles_test.rb
@@ -18,6 +18,22 @@ class BottlesTest < Minitest::Test
     assert_equal expected, Bottles.new.verse(3)
   end
 
+  def test_verse_7
+    expected = "7 bottles of beer on the wall, " +
+      "7 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 six-pack of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verse(7)
+  end
+
+  def test_verse_6
+    expected = "1 six-pack of beer on the wall, " +
+      "1 six-pack of beer.\n" +
+      "Take one down and pass it around, " +
+      "5 bottles of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verse(6)
+  end
+
   def test_verse_2
     expected = "2 bottles of beer on the wall, " +
       "2 bottles of beer.\n" +
@@ -352,9 +368,9 @@ class BottlesTest < Minitest::Test
       Take one down and pass it around, 7 bottles of beer on the wall.
 
       7 bottles of beer on the wall, 7 bottles of beer.
-      Take one down and pass it around, 6 bottles of beer on the wall.
+      Take one down and pass it around, 1 six-pack of beer on the wall.
 
-      6 bottles of beer on the wall, 6 bottles of beer.
+      1 six-pack of beer on the wall, 1 six-pack of beer.
       Take one down and pass it around, 5 bottles of beer on the wall.
 
       5 bottles of beer on the wall, 5 bottles of beer.
