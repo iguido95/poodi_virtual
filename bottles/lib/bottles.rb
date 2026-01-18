0745b70c69050e9730dd41153537047102b4aeba
Guido <iguido@me.com>
2026-01-18 18:12:59 +0100

    refactored with flocking rules

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,29 +1,53 @@
 class Bottles
-      def song
-        verses(99, 0)
-      end
+  def song
+    verses(99, 0)
+  end
 
-      def verses(upper, lower)
-        upper.downto(lower).map { |i| verse(i) }.join("\n")
-      end
+  def verses(upper, lower)
+    upper.downto(lower).map { |i| verse(i) }.join("\n")
+  end
 
-      def verse(number)
-        case number
-        when 0
-          "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-        when 1
-          "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-        else
-          "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
-        end
-      end
+  def verse(number)
+    "#{no_of_containers(number).capitalize} of beer on the wall, #{no_of_containers(number)} of beer.\n#{action(number)}, #{no_of_containers(next_number(number))} of beer on the wall.\n"
+  end
 
-      def container(number)
-        if number == 1
-          "bottle"
-         else
-          "bottles"
-         end
-      end
-
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{container_reference(number)} down and pass it around"
     end
+  end
+
+  def next_number(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
+
+  def no_of_containers(number)
+    if number == 0
+      "no more #{container(number)}"
+    else
+      "#{number} #{container(number)}"
+    end
+  end
+
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+       "bottles"
+    end
+  end
+
+  def container_reference(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+end
