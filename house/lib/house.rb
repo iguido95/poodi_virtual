a684e8a9ee77ff056fd00b9976388d6258155eab
Guido <iguido@me.com>
2026-01-21 11:03:12 +0100

    my shameless green

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,3 +1,37 @@
 class House
 
+  def recite
+    (1..12).to_a.map { |n| line(n) }.join("\n")
+  end
+
+  def line(number)
+    case number
+    when 1
+      "This is the house that Jack built."
+    when 2
+      "This is the malt that lay in the house that Jack built."
+    when 3
+      "This is the rat that ate the malt that lay in the house that Jack built."
+    when 4
+      "This is the cat that killed the rat that ate the malt that lay in the house that Jack built."
+    when 5
+      "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built."
+    when 6
+      "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built."
+    when 7
+      "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built."
+    when 8
+      "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built."
+    when 9
+      "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built."
+    when 10
+      "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built."
+    when 11
+      "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built."
+    when 12
+      "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built."
+    else
+      ""
+    end + "\n"
+  end
 end
